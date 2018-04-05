3af9f16218b3b957115846b1993fa2ab5ffd4bd8
Simon Seo <simonseo.doubles@gmail.com>
2018-04-05 11:33:31 +0400

    Update duo.py

--- duo/duo.py
+++ duo/duo.py
@@ -4,21 +4,6 @@ import base64
 import json
 import sys
 from urllib import parse
-# Provide the main function with the url to the reactivation QR code. Example:
-# "https://api-e4c9863e.duosecurity.com/frame/qr?value=c53Xoof7cFSOHGxtm69f-YXBpLWU0Yzk4NjNlLmR1b3NlY3VyaXR5LmNvbQ"
-# 
-# How to get QR code:
-# log in on start.nyu.edu
-# click "NYU Multi-Factor Authentication Registration & Update"
-# you will see a list of your pre-registered devices
-# if you don't have any MFA devices, click "Add a new device" and follow instructions
-# assuming you have one, click "My Settings & Devices"
-# select your device from the list and click "Device Options" > "Reactivate Duo Mobile"
-# select options that match your device and click "I have Duo Mobile installed"
-# right click on the QR code that shows up and copy image address 
-# 
-# The counter for HOTP (counter based) will start from 0.
-# Duo doesn't seem to support TOTP (time based).
 
 def main(qr_url):
 	data = parse.unquote(qr_url.split('=')[1])                 # get ?value=XXX
@@ -27,7 +12,7 @@ def main(qr_url):
 	host = base64.b64decode(hostb64 + '='*(-len(hostb64) % 4)) # Same as "api-e4c9863e.duosecurity.com"
 
 
-	url = 'https://{host}/push/v2/activation/{code}'.format(host=host.decode("utf-8"), code=code)
+	url = 'https://{host}/push/v2/activation/{code}'.format(host=host.decode("utf-8"), code=code) # this api is not publicly known
 	print(url)
 	response = requests.post(url)
 	body = json.loads(response.text)
