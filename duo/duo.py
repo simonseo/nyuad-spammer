4263effcf69e16b772a7b30a900ff66288371b56
Simon Myunggun Seo <ms9144@nyu.edu>
2018-04-11 01:59:29 +0400

    Error handling and modularization in duo.py

--- duo/duo.py
+++ duo/duo.py
@@ -5,20 +5,32 @@ import json
 import sys
 from urllib import parse
 
-def main(qr_url):
-	data = parse.unquote(qr_url.split('=')[1])                 # get ?value=XXX
+def qr_url_to_secret(qr_url):
+	#--- Create request URL
+	data = parse.unquote(qr_url.split('?value=')[1])           # get ?value=XXX
 	code = data.split('-')[0].replace('duo://', '')            # first half of value is the activation code
 	hostb64 = data.split('-')[1]                               # second half of value is the hostname in base64
 	host = base64.b64decode(hostb64 + '='*(-len(hostb64) % 4)) # Same as "api-e4c9863e.duosecurity.com"
-
-
 	url = 'https://{host}/push/v2/activation/{code}'.format(host=host.decode("utf-8"), code=code) # this api is not publicly known
 	print(url)
-	response = requests.post(url)
-	body = json.loads(response.text)
-	print(body)
 
-	hotp_secret = body['response']['hotp_secret']
+	#--- Get response which will be a JSON of secret keys, customer names, etc.
+	#--- Expected Response: {'response': {'hotp_secret': 'blahblah123', ...}, 'stat': 'OK'}
+	#--- Expected Error: {'code': 40403, 'message': 'Unknown activation code', 'stat': 'FAIL'}
+	response = requests.post(url)
+	response_dict = json.loads(response.text)
+	if response_dict['stat'] == 'FAIL':
+		raise Exception("The given URL is invalid. Try a new QR URL: {}".format(qr_url))
+	print(response_dict)
+
+	#--- Save to secrets.json
+	with open("secrets.json", "w") as f:
+		secrets = {}
+		secrets["hotp_secret"] = response_dict['response']['hotp_secret']
+		secrets["count"] = 0
+		json.dump(secrets, f)
+
+	hotp_secret = response_dict['response']['hotp_secret']
 	secret = base64.b32encode(hotp_secret.encode("utf-8"))
 	print("HOTP Secret:", secret) # As long as the secret key is the same, 
 	hotp = pyotp.HOTP(secret)     # the HOTP object is the same
@@ -29,8 +41,31 @@ def main(qr_url):
 	for i in range(10):
 		print(hotp.at(i))
 
+	return secret
+
+def HOTP():
+	'''Usage: generate = HOTP(); passcode = generate()'''
+	#--- Create HOTP object
+	with open("secrets.json", "r") as f:
+		secret_dict = json.load(f)
+		secret = secret_dict.get("hotp_secret")
+	secret = base64.b32encode(secret.encode("utf-8"))
+	hotp = pyotp.HOTP(secret)
+
+	#--- Generate new passcode
+	def generate():
+		count = secret_dict.get("count", 0)
+		passcode = hotp.at(count)
+		secret_dict["count"] = count + 1
+		with open("secrets.json", "w") as f:
+			secret_dict = json.dump(secret_dict, f)
+		return passcode
+	return generate
+
+
 if __name__ == '__main__':
 	if len(sys.argv) < 2:
-		print("Usage: python3 duo.py <url-to-duo-qr>"); exit()
+		print("Usage: python3 duo.py <url-to-duo-qr>")
+		exit()
 	qr_url = sys.argv[1]
-	main(qr_url)
+	qr_url_to_secret(qr_url)
