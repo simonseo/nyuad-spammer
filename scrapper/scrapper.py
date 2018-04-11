7be26c522c4936378bf22877baf2a41d0d30448c
Simon Myunggun Seo <ms9144@nyu.edu>
2018-04-11 04:54:18 +0400

    modularize scrapper to simplify scrapping other pages

--- scrapper/scrapper.py
+++ scrapper/scrapper.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*- 
 # @File Name: scrapper.py
 # @Created:   2018-04-11 02:57:12  Simon Myunggun Seo (simon.seo@nyu.edu) 
-# @Updated:   2018-04-11 04:45:02  Simon Seo (simon.seo@nyu.edu)
+# @Updated:   2018-04-11 04:53:17  Simon Seo (simon.seo@nyu.edu)
 import sys, time
 sys.path.insert(0,'..')
 from duo import duo
@@ -12,14 +12,14 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup as bs
 
-def getAnnouncementJson(nyu_netid, nyu_password):
+def setupHeadlessDriver():
 	options = webdriver.ChromeOptions()
 	options.add_argument('headless')
 	driver = webdriver.Chrome(chrome_options=options)
 	driver.implicitly_wait(10)
-	main_window = driver.current_window_handle
-	driver.get("https://students.nyuad.nyu.edu/apps/announcements/index")
+	return driver
 
+def authenticate(driver, nyu_netid, nyu_password):
 	if "NYU Login" in driver.title:
 		# Normal Auth
 		print("Authenticating NYU Shibboleth")
@@ -31,7 +31,7 @@ def getAnnouncementJson(nyu_netid, nyu_password):
 
 		password_box.send_keys(Keys.RETURN)
 		time.sleep(5)
-	if "NYU Login" in driver.title:
+
 		# Duo MFA
 		print("Authenticating Duo MFA")
 		driver.switch_to_frame(driver.find_element_by_id("duo_iframe"))
@@ -48,12 +48,18 @@ def getAnnouncementJson(nyu_netid, nyu_password):
 		driver.switch_to_default_content()
 		time.sleep(3)
 
+def getAnnouncementJson(driver, nyu_netid, nyu_password):
+	print("Retrieving Student Portal Announcements")
+	driver.get("https://students.nyuad.nyu.edu/apps/announcements/index")
+	if "NYU Login" in driver.title:
+		authenticate(driver, nyu_netid, nyu_password)
 	jsonStr = driver.find_element_by_tag_name('body').text
-	driver.close()
 	return jsonStr
 
 if __name__ == '__main__':
 	from secrets import NYU_NETID, NYU_PASSWORD
-	jsonStr = getAnnouncementJson(NYU_NETID, NYU_PASSWORD)
+	driver = setupHeadlessDriver()
+	jsonStr = getAnnouncementJson(driver, NYU_NETID, NYU_PASSWORD)
 	print(jsonStr[:500])
+	driver.close()
 	
