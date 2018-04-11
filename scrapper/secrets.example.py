51b6c6bef665356375e4998cf7f0102f8a5c647d
Simon Myunggun Seo <ms9144@nyu.edu>
2018-04-11 04:24:24 +0400

    clean up directory

--- scrapper/secrets.example.py
+++ scrapper/secrets.example.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*- 
-# @File Name: secrets.py
+# @File Name: secrets.example.py
 # @Created:   2018-04-11 03:22:10  Simon Myunggun Seo (simon.seo@nyu.edu) 
-# @Updated:   2018-04-11 04:12:59  Simon Seo (simon.seo@nyu.edu)
+# @Updated:   2018-04-11 04:24:04  Simon Seo (simon.seo@nyu.edu)
 
+# Rename to secrets.py
 NYU_NETID = 'username'
 NYU_PASSWORD = 'password'
