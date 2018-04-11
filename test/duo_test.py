56dc6fbb7702694cebf3adb2d291c1d33c2a125e
Simon Myunggun Seo <ms9144@nyu.edu>
2018-04-12 00:28:47 +0400

    Fix styling errors

--- test/duo_test.py
+++ test/duo_test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*- 
-# @File Name: duo.test.py
+# @File Name: duo_test.py
 # @Created:   2018-04-12 00:11:28  Simon Myunggun Seo (simon.seo@nyu.edu) 
-# @Updated:   2018-04-12 00:13:26  Simon Seo (simon.seo@nyu.edu)
+# @Updated:   2018-04-12 00:27:11  Simon Seo (simon.seo@nyu.edu)
 
 # from duo import duo
--- test/test_unittest.py
+++ test/test_unittest.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*- 
 # @File Name: test_unittest.py
 # @Created:   2018-04-12 00:11:44  Simon Myunggun Seo (simon.seo@nyu.edu) 
-# @Updated:   2018-04-12 00:21:39  Simon Seo (simon.seo@nyu.edu)
+# @Updated:   2018-04-12 00:27:14  Simon Seo (simon.seo@nyu.edu)
 
 import pytest
 
