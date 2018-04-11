cfcc65712a463bb402da29939855f1f345c10c92
arantzardzm <arantzardzm@hotmail.com>
2018-04-12 00:30:43 +0400

    improved bot chatting

--- bot/index.js
+++ bot/index.js
@@ -3,18 +3,17 @@ const config = require('config');
 const TeleBot = require('telebot');
 
 const bot = new TeleBot({
-
 	token: config.get('access_token'),
 	usePlugins: ['about'],
 	pluginFolder: '../../../plugins/'
-
 });
-
 
 bot.on(['/start'], (msg) => {
 	return bot.sendMessage(
-		msg.from.id, `Hello, ${ msg.from.first_name }! Welcome to the NYUAD Spammer Chatbot`
-	);
+		msg.from.id, `Hello, ${ msg.from.first_name }!` 
+	).then(re => {
+		bot.sendMessage(msg.from.id, 'Welcome to the NYUAD Spammer Bot');
+	});
 });
 
 bot.start();
--- bot/plugins/about.js
+++ bot/plugins/about.js
@@ -3,21 +3,30 @@ module.exports =  {
 	id: 'about',
 	defaultConfig: {
 		messageTypes: ['text'],
-		message: 'This bot was developed as part of the Software Engineering project. It has the purpose of providing students with information from Student Portal.'
 	},
 
 	plugin(bot, pluginConfig) {
 
 		bot.on('text', (msg) => {
 
-			if (msg.text == 'about' || msg.text == 'About'){
-				return bot.sendMessage(
-					msg.from.id, pluginConfig.message
-				);
-			}
+		let message = 'This bot was developed as part of the Software Engineering class.'
+		let message_1 = 'It has the purpose of helping students access Student Portal faster and more efficiently.'
+		let message_2 = 'Please type "/commands" or "commands" to see what type of information is available.';
+
+			if (msg.text == 'about' || msg.text == 'About' || msg.text == '/about'){
+
+				return bot.sendMessage(msg.chat.id, message).then(() => {
+					bot.sendMessage(msg.chat.id, message_1).then(() => {
+						bot.sendMessage(msg.chat.id, message_2);
+					});
+				});
+
+			};
 
 		});
+
 	}
+
 };
 
 
