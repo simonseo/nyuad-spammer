e8e1def77fafbc0caabd8096a5b5743ae1895a63
Khaled AlHosani <kah579@nyu.edu>
2018-04-11 22:35:14 +0400

    See previous commit: Only new, not updated files were pushed

--- flask/dbsetup.py
+++ flask/dbsetup.py
@@ -4,10 +4,37 @@ conn = sqlite3.connect('posts.db')
 c = conn.cursor()
 
 c.execute("DROP TABLE IF EXISTS sample")
+c.execute("DROP TABLE IF EXISTS posts")
 
 c.execute("""CREATE TABLE posts (
-	ID integer primary key,
-	content text not null
+	alert text not null,
+	alert_end text not null,
+	alert_end_date text not null,
+	alert_end_time text not null,
+	alert_start text not null,
+	alert_start_date text not null,
+	alert_start_time text not null,
+	audience_0 text not null,
+	audience_1 text not null,
+	audience_2 text not null,
+	category_id text not null,
+	category_name text not null,
+	created_at text not null,
+	editable text not null,
+	email text not null,
+	expiration_date text not null,
+	fullname text not null,
+	ID text primary key,
+	location_0 text not null,
+	location_1 text not null,
+	message text not null,
+	owner text not null,
+	publish_date text not null,
+	republished_at text not null,
+	title text not null,
+	topic text not null,
+	updated text not null,
+	updated_at text not null
 )""")
 
 conn.commit()
--- flask/flaskapp.py
+++ flask/flaskapp.py
@@ -1,19 +1,24 @@
-from flask import Flask
+from flask import Flask, request, redirect, url_for, send_from_directory
 from post import Post
-import sqlite3 
 from secret import HOSTADDRESS
+from werkzeug import secure_filename
+from readCSV import ReadCSV
 import databaseOperations as db
+import sqlite3 
+import os
+
+UPLOAD_FOLDER = 'uploads'
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route("/drop")
-def drop():
-	return db.drop()
+@app.route("/printall")
+def printall():
+	return db.printall()
 
-@app.route("/addpost/<postToAdd>")
-def addpost(postToAdd):
-	return db.addpost(postToAdd)
-
+# @app.route("/addpost/<int:IDToAdd>/<postToAdd>")
+# def addpost(IDToAdd,postToAdd):
+# 	return db.addpost(IDToAdd,postToAdd)
 
 @app.route("/getpost/<int:getID>")
 def getpost(getID):
@@ -22,6 +27,31 @@ def getpost(getID):
 @app.route("/rmpost/<int:rmID>")
 def removepost(rmID):
 	return db.removepost(rmID)
+
+@app.route("/", methods=['GET','POST'])
+def getCSV():
+	# http://flask.pocoo.org/docs/patterns/fileuploads/
+	if request.method == 'POST':
+		if 'file' not in request.files:
+			return "No file part"
+		file = request.files['file']
+		if file.filename == '':
+			return "No selected file"
+		if file and (file.filename[-3:].lower() == 'csv'):
+			filename = secure_filename(file.filename)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			db.injectData(ReadCSV(filename))
+			return "upload successful"
+
+	return '''
+	<!doctype html>
+	<title>Upload new File</title>
+	<h1>Upload new File</h1>
+	<form method=post enctype=multipart/form-data>
+	  <p><input type=file name=file>
+		 <input type=submit value=Upload>
+	</form>
+	'''
 
 if __name__ == "__main__":
 	app.run()#host=HOSTADDRESS, port=80)
