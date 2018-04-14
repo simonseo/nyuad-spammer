753ebf9973cc3093467380c817addc6c067fe991
arantzardzm <arantzardzm@hotmail.com>
2018-04-15 01:17:38 +0400

    changed intro message

--- bot/plugins/about.js
+++ bot/plugins/about.js
@@ -9,15 +9,27 @@ module.exports =  {
 
   plugin(bot, pluginConfig) {
 
-    bot.on('text', (msg) => {
+    bot.on('/about', (msg) => {
 
-      let message = 'This bot was developed as part of the Software Engineering class. ' +
-      'It has the purpose of helping students access Student Portal more efficiently. ' +
-      'Please type "/commands" or "commands" to see what type of information is available.';
+      let parseMode = 'html';
+      let message = `Hello, ${ msg.from.first_name },` + '\n\n' +
+      'I see you have finally decided to try me out. Let me tell you that you have ' +
+      'made the right decision!' + '\n\n' +
+      '<b>About</b>' + '\n' +
+      'With me, you will always be the first one at NYUAD to be informed of ' +
+      'upcoming deadlines, current events, modified dining hall hours, new ticket ' +
+      'sales, and much more.' + '\n\n' +
+      '<b>Announcements and Subscriptions</b>' + '\n' +
+      'If you wish to read about current announcements or subscribe to notifications, ' +
+      'simply go to our menu by typing: /menu ' + '\n' +
+      'Select your desired option and then type the name of a Student Portal ' +
+      'category: /studentlife' + '\n\n' +
+      '<b>Categories</b>' + '\n' +
+      'To view the list of available Student Portal categories I currently have, ' +
+      'please type: /categories.' + '\n\n' +
+      'Enjoy!';
 
-       if (msg.text.toLowerCase() == 'about' || msg.text.toLowerCase() == '/about'){
-          return bot.sendMessage(msg.from.id, message);
-        };
+      return bot.sendMessage(msg.from.id, message, {parseMode});
 
     });
   }
