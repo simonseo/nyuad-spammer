498dbd5c2f7bb87ebd3c982b6b0f3ea80a4ce9cf
arantzardzm <arantzardzm@hotmail.com>
2018-04-14 18:36:17 +0400

    minor modifications

--- bot/index.js
+++ bot/index.js
@@ -1,19 +1,22 @@
-const config = require('config');
+'use strict';
 
+const config = require('config');
 const TeleBot = require('telebot');
 
 const bot = new TeleBot({
-	token: config.get('access_token'),
-	usePlugins: ['about'],
-	pluginFolder: '../../../plugins/'
+  token: config.get('access_token'),
+  usePlugins: ['about', 'dialogflow'],
+  pluginFolder: '../../../plugins/'
 });
 
-bot.on(['/start'], (msg) => {
-	return bot.sendMessage(
-		msg.from.id, `Hello, ${ msg.from.first_name }!` 
-	).then(re => {
-		bot.sendMessage(msg.from.id, 'Welcome to the NYUAD Spammer Bot');
-	});
+bot.on('text', (msg) => {
+
+  if (msg.text.toLowerCase() == 'start' || msg.text.toLowerCase() == '/start'){
+    return bot.sendMessage(msg.from.id, `Hello, ${ msg.from.first_name }!`).then(() => {
+        bot.sendMessage(msg.from.id, 'Welcome to the NYUAD Spammer Bot');
+    });
+  }
+
 });
 
 bot.start();
