40752e7517315ec05233a737d565cbe8d929b493
Simon Myunggun Seo <ms9144@nyu.edu>
2018-04-18 17:59:01 +0400

    change folder structure to fit package model

--- __init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*- 
-# @File Name: __init__.py
-# @Created:   2018-04-17 10:59:47  Simon Myunggun Seo (simon.seo@nyu.edu) 
-# @Updated:   2018-04-17 11:00:01  Simon Seo (simon.seo@nyu.edu)
-
-from duo import duo
-from scrapper import scrapper
--- scrapper/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*- 
-# @File Name: __init__.py
-# @Created:   2018-04-17 11:00:13  Simon Myunggun Seo (simon.seo@nyu.edu) 
-# @Updated:   2018-04-18 17:07:57  Simon Seo (simon.seo@nyu.edu)
-
-from scrapper import scrapper
