c2cc0d81a65472e176f6624de239ea0bcbeced33
Khaled AlHosani <kah579@nyu.edu>
2018-04-29 23:39:20 +0400

    made userID unique in DB, changed to focus on topics instead of categories. topic IDs given arbritraily but will be used userSubscriptions

--- flask/dbsetup.py
+++ flask/dbsetup.py
@@ -21,23 +21,24 @@ def DBsetup():
 	)""")
 
 	c.execute("""CREATE TABLE IF NOT EXISTS users (
-		userID text not null
+		userID text not null unique,
+		last_updated text not null
 	)""")
 
 	c.execute("""CREATE TABLE IF NOT EXISTS userSubscriptions (
-		category_id text not null,
+		topic_id text not null,
 		userID text not null
 	)""")
 
-	c.execute("""CREATE TABLE IF NOT EXISTS categories (
-		category_id text not null,
-		category_name text not null
+	c.execute("""CREATE TABLE IF NOT EXISTS topics (
+		topic_id text not null,
+		topic text not null
 	)""")
 
-	categories= dict([("Events & Activities", 1),("News & Information", 5),("Learning & Development", 2),("Service Notifications", 4),("Job Opportunities, Internships & Volunteering", 7),("Deadlines", 3),("Discounts, Deals & Promotions", 6),("Policies", 9)])
-
-	for k, v in categories.items():
-		c.execute("INSERT INTO categories VALUES (:category_id, :category_name)", {"category_id":v, "category_name":k})
+	topics= dict( [("Academics", 1), ("Environmental Health and Safety", 2), ("Intercultural Affairs", 3), ("Events and Activities", 4), ("Student Activities", 5), ("Dining", 6), ("Community Outreach", 7), ("Fitness Center", 8), ("Research", 9), ("Campus Life", 10), ("Athletics", 11), ("Registrar", 12), ("Community Life", 13), ("First-Year Office", 14), ("Career Development", 15), ("Academic Affairs", 16), ("Spiritual Life", 17), ("Library", 18), ("Communications", 19), ("Finance", 20), ("NYUAD Services", 21), ("Residential Education", 22), ("Global Education", 23), ("Mentoring", 24), ("Admissions", 25), ("Around Abu Dhabi", 26), ("Grants", 27), ("Bookstore", 28), ("Facilities", 29), ("Health and Welness", 30), ("Housing", 31), ("Human Resources", 32), ("Mail", 33), ("Public Safety", 34), ("Technology", 35), ("Transportation", 36), ("Travel", 37), ("Operations", 38), ("Students Abroad", 39)])
+		
+	for k, v in topics.items():
+		c.execute("INSERT INTO topics VALUES (:topic_id, :topic)", {"topic_id":v, "topic":k})
 
 	conn.commit()
 	conn.close()
