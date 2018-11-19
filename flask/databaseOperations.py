378288653e00cce1da1ec5696eca86019b400446
Khaled AlHosani <kah579@nyu.edu>
2018-11-19 18:14:25 +0400

    Flask branch - change updated_at as time it was scraped instead of time it was posted (#51)
    * current time added to new and updated posts
    
    * users' last_updated will be updated even when they have no subscriptions

--- flask/databaseOperations.py
+++ flask/databaseOperations.py
@@ -23,11 +23,11 @@ def addpost(data):
 			if toAddTime > savedTime:
 				print("Time replaced for post ID "+ postToAdd.ID)
 				with conn:
-					c.execute("UPDATE posts SET message=?, updated_at=? WHERE ID=?", (postToAdd.message, postToAdd.updated_at, postToAdd.ID))
+					c.execute("UPDATE posts SET message=?, updated_at=? WHERE ID=?", (postToAdd.message, time.strftime("%Y-%m-%d %H:%M:%S"), postToAdd.ID))
 
 	else:
 		with conn:
-			c.execute("INSERT INTO posts VALUES (:category_id, :category_name, :created_at, :email, :fullname, :ID, :message, :publish_date, :title, :topic, :updated, :updated_at)", {"category_id":postToAdd.category_id, "category_name":postToAdd.category_name, "created_at":postToAdd.created_at, "email":postToAdd.email, "fullname":postToAdd.fullname, "ID":postToAdd.ID, "message":postToAdd.message, "publish_date":postToAdd.publish_date, "title":postToAdd.title, "topic":postToAdd.topic, "updated":postToAdd.updated, "updated_at":postToAdd.updated_at})
+			c.execute("INSERT INTO posts VALUES (:category_id, :category_name, :created_at, :email, :fullname, :ID, :message, :publish_date, :title, :topic, :updated, :updated_at)", {"category_id":postToAdd.category_id, "category_name":postToAdd.category_name, "created_at":postToAdd.created_at, "email":postToAdd.email, "fullname":postToAdd.fullname, "ID":postToAdd.ID, "message":postToAdd.message, "publish_date":postToAdd.publish_date, "title":postToAdd.title, "topic":postToAdd.topic, "updated":postToAdd.updated, "updated_at":time.strftime("%Y-%m-%d %H:%M:%S")})
 
 	conn.commit()
 	conn.close()
